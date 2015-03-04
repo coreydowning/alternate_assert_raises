9694e6d8970c75dad498a065fa589f761cb2b684
Arjun Sharma <asharma@pivotal.io>
2015-03-04 12:55:29 -0700

    Example code demonstrating the alternate assertRaises

---
+++ test_example.py
@@ -0,0 +1,34 @@
+import sys
+import unittest
+
+class OtherException(Exception):
+    pass
+
+class RealException(Exception):
+    pass
+
+def function_under_test():
+    raise RealException('hey!')
+
+class CurrentAssertRaisesTest(unittest.TestCase):
+    def test_passing(self):
+        self.assertRaises(RealException, function_under_test)
+
+    def test_failing(self):
+        self.assertRaises(OtherException, function_under_test)
+
+class NewAssertRaisesTest(unittest.TestCase):
+    def alternateAssertRaises(self, excClass, callableObj, *args, **kwargs):
+        try:
+            callableObj(*args, **kwargs)
+        except excClass:
+            return
+        except:
+            e = sys.exc_info()[1]
+            raise self.failureException('Expected to raise "%s", actually raised "%s"' % (excClass.__name__, e.__class__.__name__))
+
+    def test_passing(self):
+        self.alternateAssertRaises(RealException, function_under_test)
+
+    def test_failing(self):
+        self.alternateAssertRaises(OtherException, function_under_test)
