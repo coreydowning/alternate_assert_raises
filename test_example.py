4bc10c84e6f2224b4bd562c11dbcec6d850ba29b
Corey Downing <downing.c@gmail.com>
2015-03-04 21:16:40 -0700

    Fail when function under test does not raise any exception

--- test_example.py
+++ test_example.py
@@ -10,6 +10,9 @@ class RealException(Exception):
 def function_under_test():
     raise RealException('hey!')
 
+def working_function():
+    pass
+
 class CurrentAssertRaisesTest(unittest.TestCase):
     def test_passing(self):
         self.assertRaises(RealException, function_under_test)
@@ -26,9 +29,14 @@ class NewAssertRaisesTest(unittest.TestCase):
         except:
             e = sys.exc_info()[1]
             raise self.failureException('Expected to raise "%s", actually raised "%s"' % (excClass.__name__, e.__class__.__name__))
+        else:
+            raise self.failureException('Expected to raise "%s" but raised nothing' % (excClass.__name__))
 
     def test_passing(self):
         self.alternateAssertRaises(RealException, function_under_test)
 
     def test_failing(self):
         self.alternateAssertRaises(OtherException, function_under_test)
+
+    def test_also_failing(self):
+        self.alternateAssertRaises(RealException, working_function)
